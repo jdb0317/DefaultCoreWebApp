0dba44ec5d393d5211ae29a86f4484a476d2b280
jdb0317 <jonathanbrown0123@gmail.com>
2025-08-24 12:57:34 -0500

    first commit

---
+++ DefaultCoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DefaultCoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
